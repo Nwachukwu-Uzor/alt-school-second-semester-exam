623502e72955ed8d14899b53aded16e19021ba1a
¨Nwachukwu-Uzor¨ <uzor1997@gmail.com>
2025-06-10 11:05:21 +0100

    add set up to server static second semester exams

--- server/app.js
+++ server/app.js
@@ -1,17 +1,26 @@
 import express from "express";
-import cors from "cors"
-import 'dotenv/config'
+import cors from "cors";
+import path from "path";
+import "dotenv/config";
 
 const PORT = process.env.PORT;
+const LANDING_PAGE_PATH = process.env.LANDING_PAGE_PATH;
 
 const app = express();
-app.use(cors())
-app.use(express.urlencoded({}))
+app.use(cors());
+app.use(express.urlencoded({extended: true}));
 
-app.listen(PORT, () => {
-    console.log(`app is listening on ${PORT}`)
-})
+app.use("/second-semester-exam", express.static(LANDING_PAGE_PATH));
+
+app.get("/second-semester-exam/{*any}", (_req, res) => {
+  res.sendFile(path.join(LANDING_PAGE_PATH, "index.html"));
+});
 
 app.get("/", (_req, res) => {
-    return res.json({message: "Ok"})
-})
+  return res.json({ message: "Ok" });
+});
+
+app.listen(PORT, () => {
+  console.log(`app is listening on ${PORT}`);
+});
+
